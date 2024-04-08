fbed9a90825cabe8d925cf25367335a08856efbd
andrewmei <amei01@calpoly.edu>
2024-04-08 09:39:19 -0700

    Adding IE1

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,43 @@
+// src/MyApp.jsx
+import React, { useState } from "react";
+import Table from "./Table";
+
+function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]);
+
+  // filter function -- makes a new array where it does not have the one you wanted to remove
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
+  return (
+    <div className="container">
+      <Table 
+        characterData={characters} 
+        removeCharacter={removeOneCharacter}
+      />
+    </div>
+  );
+}
+
+export default MyApp;
